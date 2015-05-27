cbbc9636f108def80db5ab239cd3913c8a495c33
mpmendespt <mpmendespt@yahoo.com>
2015-05-28 00:44:13 +0100

    Test2...

---
+++ _drafts/Code1.R
@@ -0,0 +1,9 @@
+
+
+filename = "Hello-World.Rmd"
+source("r2jekyll.R")
+filename = "Test1.Rmd"
+source("r2jekyll.R")
+filename = "whats-jekyll.Rmd"
+source("r2jekyll.R")
+
---
+++ _posts/2015-05-28-Hello-World.md
@@ -0,0 +1,8 @@
+---
+title: "Hello World"
+author: "MM"
+date: "Wednesday, May 27, 2015"
+layout: post
+---
+
+## "Hello World""
