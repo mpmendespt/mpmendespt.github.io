d0b3efd763ada05b41025b61358a5450cef87782
mpmendespt <mpmendespt@yahoo.com>
2015-05-28 21:19:45 +0100

    Test11...

--- _drafts/Code1.R
+++ _drafts/Code1.R
@@ -1,9 +1,11 @@
 
-
+setwd("D:/DL/_DL_/_ELECTRONICA_/_ESTUDO_3/_Blog with RStudio_/mpmendespt.github.io/_drafts")
 filename = "Hello-World.Rmd"
 source("r2jekyll.R")
-filename = "Test1.Rmd"
-source("r2jekyll.R")
+
 filename = "whats-jekyll.Rmd"
+source("r2jekyll.R")
+
+filename = "Test1.Rmd"
 source("r2jekyll.R")
 
--- _drafts/r2jekyll.R
+++ _drafts/r2jekyll.R
@@ -18,8 +18,8 @@ output = paste0(dir, sub('.Rmd', '.md', filename))
 knit(filename, output)
 
 # Copy .png files to the images directory.
-fromdir = "{{ site.url }}/images"
-todir = "../images"
+fromdir = "./figure"
+todir = "../figure"
 
 pics = list.files(fromdir, ".png")
 pics = sapply(pics, function(x) paste(fromdir, x, sep="/"))
