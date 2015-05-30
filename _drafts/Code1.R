6814061339a4bea64c560f1acf4a03a395696a29
mpmendespt <mpmendespt@yahoo.com>
2015-05-30 20:53:44 +0100

    PostX...

--- _drafts/Code1.R
+++ _drafts/Code1.R
@@ -10,3 +10,5 @@ filename = "Test1.Rmd"
 source("r2jekyll.R")
 
 setwd("D:/DL/_DL_/_ELECTRONICA_/_ESTUDO_3/_Blog with RStudio_/mpmendespt.github.io/_drafts")
+filename = "PA1_Statistical_Inference.Rmd"
+source("r2jekyll.R")
